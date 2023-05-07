86e6d9afd753223decb697babdaa7f2e7195413e
Marcos <marcos.alloza4@gmail.com>
2023-05-07 12:36:09 +0200

    project creation

---
+++ src/main/java/com/burgueranteux/AnteuxApplication.java
@@ -0,0 +1,13 @@
+package com.burgueranteux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnteuxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnteuxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/burgueranteux/AnteuxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.burgueranteux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnteuxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
