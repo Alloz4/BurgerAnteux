349b6828acb4aee67b28320d94e09fbf514344d1
gonzsanz <davidgonzsanz02@gmail.com>
2023-05-11 18:53:51 +0200

    Repository add

---
+++ src/main/java/com/burgeranteux/repository/Detail.java
@@ -0,0 +1,6 @@
+package com.burgeranteux.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface Detail extends JpaRepository<Detail, Long> {
+}
---
+++ src/main/java/com/burgeranteux/service/UserService.java
@@ -0,0 +1,20 @@
+package com.burgeranteux.service;
+
+//import com.burgeranteux.repository.UserRepository;
+
+import java.util.Optional;
+
+public class UserService {
+
+//    UserRepository userRepository;
+
+//    public UserService(UserRepository userRepository) {
+//        this.userRepository = userRepository;
+//    }
+//
+//    public User createUser(User user) {
+//        return userRepository.save(user);
+//    }
+//
+//    public
+}
